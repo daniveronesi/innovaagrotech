23872fbd8677db7520f10a8dd517312d54291097
murarirc <raphaelmcaceres@gmail.com>
2024-10-13 13:54:36 -0300

    instalado react e firebase. realizado limpeza

---
+++ src/firebase/index.js
@@ -0,0 +1,24 @@
+
+import { initializeApp } from "firebase/app"
+import { getFirestore } from "firebase/firestore"
+import { getAuth } from "firebase/auth"
+
+//import { getAnalytics } from "firebase/analytics"
+
+const firebaseConfig = {
+  apiKey: "AIzaSyB7ZkmKJIWPJA6kAM407_R0bZR9p1aq6nM",
+  authDomain: "innovaagrotech-bb5c1.firebaseapp.com",
+  projectId: "innovaagrotech-bb5c1",
+  storageBucket: "innovaagrotech-bb5c1.appspot.com",
+  messagingSenderId: "600440000154",
+  appId: "1:600440000154:web:8c690df65682e3109b1923",
+  measurementId: "G-80081221P4"
+}
+
+const app = initializeApp(firebaseConfig)
+//const analytics = getAnalytics(app)
+
+const db = getFirestore(firebaseapp)
+const auth = getAuth(firebaseapp)
+
+export { auth, db }
