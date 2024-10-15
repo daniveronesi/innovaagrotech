562783480cf567272723fda4417bf17e3559a243
Tawany2004 <tawanyvick@gmail.com>
2024-10-15 18:07:34 -0300

    Elementos adicionados em Produtos junto com padronização de tipografia

--- src/App.js
+++ src/App.js
@@ -2,6 +2,7 @@ import React from 'react'
 import './App.css'
 import { BrowserRouter as Router, Route, Routes } from 'react-router-dom'
 import Home from './Pages/Home/home'
+import Produtos from './Pages/Produtos/Produto'
 import Navbar from './Components/Navbar'
 
 
@@ -14,6 +15,7 @@ function App() {
         <Navbar />
         <Routes>
           <Route path="/" element={<Home />} />
+          <Route path="Produtos" element={<Produtos />} />
         </Routes>
       </div>
     </Router>
